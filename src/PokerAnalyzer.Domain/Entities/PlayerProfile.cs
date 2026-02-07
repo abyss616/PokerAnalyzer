d9d963f1b9caffb5201ee1688e5856b0b298899a
abyss616 <47752422+abyss616@users.noreply.github.com>
2026-02-07 10:13:32 +0200

    Add river stat tracking

--- src/PokerAnalyzer.Domain/Entities/PlayerProfile.cs
+++ src/PokerAnalyzer.Domain/Entities/PlayerProfile.cs
@@ -82,14 +82,12 @@ public sealed class RiverStats
     public int SawRiver { get; set; }
     public int WentToShowdown { get; set; }
     public int WonAtShowdown { get; set; }
-    public int CBetOpportunities { get; set; }
-    public int CBets { get; set; }
-    public int FoldToCBetOpportunities { get; set; }
-    public int FoldToCBet { get; set; }
-    public int DonkBets { get; set; }
-    public int FirstFoldToCBet { get; set; }
-    public int CallVsCBet { get; set; }
-    public int RaiseVsCBet { get; set; }
-    public int MultiwayCBets { get; set; }
-    public int ProbeBets { get; set; }
+    public int RiverBetOpportunities { get; set; }
+    public int RiverBetsWhenCheckedTo { get; set; }
+    public int RiverFacedBet { get; set; }
+    public int RiverCallsVsBet { get; set; }
+    public int RiverFoldToBet { get; set; }
+    public int RiverRaiseVsBet { get; set; }
+    public decimal RiverAggressionFactor { get; set; }
+    public decimal RiverBetSizePercentPot { get; set; }
 }
