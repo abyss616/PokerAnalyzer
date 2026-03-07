373ef825af232e69394a12a8ce13d5836f4a91ad
abyss616 <teodors.berzins@gmail.com>
2026-03-07 22:28:28 +0200

    fixed test

--- tests/PokerAnalyzer.Application.Tests/PreflopRegretTrainerTests.cs
+++ tests/PokerAnalyzer.Application.Tests/PreflopRegretTrainerTests.cs
@@ -1,4 +1,5 @@
 using PokerAnalyzer.Application.PreflopSolver;
+using PokerAnalyzer.Domain.Cards;
 using PokerAnalyzer.Domain.Game;
 using Xunit;
 
@@ -68,12 +69,13 @@ public sealed class PreflopRegretTrainerTests
 
         trainer.RunIteration(new Random(13));
 
-        // No chance/leaf infosets are written since only action nodes update regrets.
         Assert.Equal(0d, regrets.Get("chance_infoset", traverser.Fold));
         Assert.Equal(0d, regrets.Get("leaf_infoset", traverser.Fold));
     }
 
+
     [Fact]
+ 
     public void RunIteration_ReplaysActionsFromStateBeforeAction()
     {
         var root = CreateHeadsUpPreflopState();
@@ -91,33 +93,64 @@ public sealed class PreflopRegretTrainerTests
         trainer.RunIteration(new Random(19));
 
         Assert.NotEmpty(traverser.RolloutRoots);
-        Assert.All(traverser.RolloutRoots, state => Assert.Equal(1, state.ActionHistory.Count));
+        Assert.All(
+            traverser.RolloutRoots,
+            state => Assert.Equal(root.ActionHistory.Count + 1, state.ActionHistory.Count));
     }
 
     private static SolverHandState CreateHeadsUpPreflopState()
     {
         var sbId = new PlayerId(Guid.Parse("11111111-1111-1111-1111-111111111111"));
         var bbId = new PlayerId(Guid.Parse("22222222-2222-2222-2222-222222222222"));
-        var config = new GameConfig(MaxPlayers: 2, SmallBlind: new ChipAmount(1), BigBlind: new ChipAmount(2), Ante: ChipAmount.Zero, StartingStack: new ChipAmount(100));
+
+        var config = new GameConfig(
+            MaxPlayers: 2,
+            SmallBlind: new ChipAmount(1),
+            BigBlind: new ChipAmount(2),
+            Ante: ChipAmount.Zero,
+            StartingStack: new ChipAmount(100));
 
         var players = new[]
         {
-            new SolverPlayerState(sbId, SeatIndex: 0, Position.SB, Stack: new ChipAmount(99), CurrentStreetContribution: new ChipAmount(1), TotalContribution: new ChipAmount(1), IsFolded: false, IsAllIn: false),
-            new SolverPlayerState(bbId, SeatIndex: 1, Position.BB, Stack: new ChipAmount(98), CurrentStreetContribution: new ChipAmount(2), TotalContribution: new ChipAmount(2), IsFolded: false, IsAllIn: false)
-        };
+        new SolverPlayerState(
+            sbId,
+            SeatIndex: 0,
+            Position: Position.SB,
+            Stack: new ChipAmount(99),
+            CurrentStreetContribution: new ChipAmount(1),
+            TotalContribution: new ChipAmount(1),
+            IsFolded: false,
+            IsAllIn: false),
+
+        new SolverPlayerState(
+            bbId,
+            SeatIndex: 1,
+            Position: Position.BB,
+            Stack: new ChipAmount(98),
+            CurrentStreetContribution: new ChipAmount(2),
+            TotalContribution: new ChipAmount(2),
+            IsFolded: false,
+            IsAllIn: false)
+    };
 
         return new SolverHandState(
-            config,
+            config: config,
             street: Street.Preflop,
             buttonSeatIndex: 0,
             actingPlayerId: sbId,
             pot: new ChipAmount(3),
             currentBetSize: new ChipAmount(2),
-            lastRaiseSize: new ChipAmount(2),
+            lastRaiseSize: new ChipAmount(1),
             raisesThisStreet: 0,
-            players,
-            actionHistory: [],
-            privateCardsByPlayer: new Dictionary<PlayerId, Domain.Cards.HoleCards>());
+            players: players,
+            actionHistory: new[]
+            {
+            new SolverActionEntry(sbId, ActionType.PostSmallBlind, new ChipAmount(1)),
+            new SolverActionEntry(bbId, ActionType.PostBigBlind, new ChipAmount(2))
+            },
+            boardCards: Array.Empty<Card>(),
+            deadCards: Array.Empty<Card>(),
+            privateCardsByPlayer: new Dictionary<PlayerId, HoleCards>());
     }
 
     private sealed class StubTrajectoryTraverser : IPreflopTrajectoryTraverser
@@ -125,6 +158,7 @@ public sealed class PreflopRegretTrainerTests
         private readonly SolverHandState _root;
         private readonly PlayerId _traversalPlayer;
         private readonly PlayerId _opponent;
+        private bool _returnedInitialTrajectory;
 
         public StubTrajectoryTraverser(SolverHandState root, PlayerId traversalPlayer, PlayerId opponent)
         {
@@ -142,41 +176,46 @@ public sealed class PreflopRegretTrainerTests
 
         public TrajectorySample SampleTrajectory(SolverHandState rootState, Random rng)
         {
-            if (rootState.ActionHistory.Count == 0)
+            if (!_returnedInitialTrajectory)
             {
-                var path = new List<VisitedNode>
-                {
-                    VisitedNode.CreateAction(
-                        depth: 0,
-                        street: Street.Preflop,
-                        actingPlayerId: _traversalPlayer,
-                        infoSetKey: "traversal_infoset",
-                        legalActions: new[] { Fold, Call },
-                        policy: new Dictionary<LegalAction, double> { [Fold] = 0.25d, [Call] = 0.75d },
-                        sampledAction: Fold,
-                        stateBeforeAction: rootState),
-                    VisitedNode.CreateAction(
-                        depth: 1,
-                        street: Street.Preflop,
-                        actingPlayerId: _opponent,
-                        infoSetKey: "opponent_infoset",
-                        legalActions: new[] { Fold, Call },
-                        policy: new Dictionary<LegalAction, double> { [Fold] = 0.5d, [Call] = 0.5d },
-                        sampledAction: Call,
-                        stateBeforeAction: rootState),
-                    VisitedNode.CreateChance(2, Street.Preflop, Street.Flop, "sample chance"),
-                    VisitedNode.CreateLeaf(3, Street.Flop, "leaf")
-                };
+                _returnedInitialTrajectory = true;
 
-                return new TrajectorySample(rootState, new Dictionary<PlayerId, double> { [_traversalPlayer] = 0d, [_opponent] = 0d }, path);
+                var path = new List<VisitedNode>
+            {
+                VisitedNode.CreateAction(
+                    depth: 0,
+                    street: Street.Preflop,
+                    actingPlayerId: _traversalPlayer,
+                    infoSetKey: "traversal_infoset",
+                    legalActions: new[] { Fold, Call },
+                    policy: new Dictionary<LegalAction, double> { [Fold] = 0.25d, [Call] = 0.75d },
+                    sampledAction: Fold,
+                    stateBeforeAction: rootState),
+
+                VisitedNode.CreateAction(
+                    depth: 1,
+                    street: Street.Preflop,
+                    actingPlayerId: _opponent,
+                    infoSetKey: "opponent_infoset",
+                    legalActions: new[] { Fold, Call },
+                    policy: new Dictionary<LegalAction, double> { [Fold] = 0.5d, [Call] = 0.5d },
+                    sampledAction: Call,
+                    stateBeforeAction: rootState.Apply(Fold)),
+
+                VisitedNode.CreateChance(2, Street.Preflop, Street.Flop, "sample chance"),
+                VisitedNode.CreateLeaf(3, Street.Flop, "leaf")
+            };
+
+                return new TrajectorySample(
+                    rootState,
+                    new Dictionary<PlayerId, double> { [_traversalPlayer] = 0d, [_opponent] = 0d },
+                    path);
             }
 
             RolloutRoots.Add(rootState);
 
-            var firstAction = rootState.ActionHistory[0].ActionType;
-            var utility = firstAction == ActionType.Fold ? 10d : 4d;
-            if (rootState.ActionHistory.Count > 1)
-                utility = 100d;
+            var lastAction = rootState.ActionHistory[^1].ActionType;
+            var utility = lastAction == ActionType.Fold ? 10d : 4d;
 
             return new TrajectorySample(
                 rootState,
